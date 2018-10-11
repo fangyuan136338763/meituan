155f4aae2b9efb37a083f735c9328ebbba7f9fde
fangyuan <136338763@qq.com>
2018-10-11 22:12:45 +0800

    second push

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import 'mint-ui/lib/style.css'
+import './lib/mui/css/mui.css'
+import './lib/mui/css/icons-extra.css'
+
 Vue.config.productionTip = false
 
 new Vue({
