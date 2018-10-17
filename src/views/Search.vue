e5e6861ccb725721847f08d0449a5ab5e4cc586e
fangyuan <136338763@qq.com>
2018-10-18 00:23:32 +0800

    search.vue--2

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="search-page">
-        <div>
-            <i></i>
-            <input type="text">
-            <span></span>
+        <div class="header">
+            <i class="mui-icon mui-icon-arrowleft"></i>
+            <input type="text" class="search-input" placeholder="请输入商家或商品名称">
+            <span class="search-btn">搜索</span>
         </div>
     </div>
 </template>
@@ -18,6 +18,29 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    .search-page{
+        background: #fff;
+    }
+    .header{
+        padding-top: 10px;
+    }
+    .mui-icon-arrowleft{
+        font-size: 40px;
+        vertical-align: middle;
+    }
+    .search-input{
+        width: 75%;
+        height: 30px;
+        background: #F2F2F2;
+        border-radius: 1px;
+        border: 0;
+    }
+    .search-btn{
+        display: inline-block;
+        width: 50px;
+        height: 30px;
+        background: #FFD161;
+        margin-bottom: 15px;
+    }
 </style>
